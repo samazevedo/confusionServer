cdde5c19bff143175c8435462aea6342bcd7eeb4
sam-azevedo <samuel.azevedo@live.com>
2021-07-06 15:12:11 -0300

    mongoose population

--- models/dishes.js
+++ models/dishes.js
@@ -4,6 +4,26 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
+
+//comments schema
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+})
 // create a Schema
 const dishSchema = new Schema({
     name: {
@@ -34,7 +54,8 @@ const dishSchema = new Schema({
     featured: {
         type: Boolean,
         default: false
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
